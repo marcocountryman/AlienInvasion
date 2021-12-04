cd653a575981651d305641e0ba74166fdbd0350b
mcountryman <countryman.marco@gmail.com>
2021-12-04 11:55:29 -0500

    added more aliens

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-const Alien = require("../scripts/alien.js")
+// import { set } from "core-js/core/dict";
+import Alien from "./scripts/alien.js"
 
 document.addEventListener("DOMContentLoaded", function () {
-    const canvasEl = documentEl.querySelector('#alien-canvas');
+    const canvasEl = document.getElementById('alien-canvas');
     const ctx = canvasEl.getContext('2d');
+
+    const alien1 = new Alien({pos: [50,20], size:[40,25],color: "pink"});
+    const alien2 = new Alien({pos: [200,20], size:[40,25],color: "pink"});
+    const alien3 = new Alien({pos: [50,50], size:[40,25],color: "pink"});
+    const alien4 = new Alien({pos: [200,50], size:[40,25],color: "pink"});
+    const alien5 = new Alien({pos: [50,80], size:[40,25],color: "pink"});
+    const alien6 = new Alien({pos: [200,80], size:[40,25],color: "pink"});
+    const aliens = [alien1,alien2,alien3,alien3]
+    alien1.draw(ctx)
+    alien2.draw(ctx)
+    alien3.draw(ctx)
+    alien4.draw(ctx)
+    alien5.draw(ctx)
+    alien6.draw(ctx)
+
+    // setInterval(alien1.draw)
+
+    // ctx.fillStyle = "white"
+    // ctx.fillRect(20, 20,25,50);
+    // console.log("hello");
 });
--- src/scripts/alien.js
+++ src/scripts/alien.js
@@ -1,11 +1,16 @@
 class Alien {
-    constructor(object) {
-        this.pos;
-        this.size;
-        this.color;
+    constructor(object,ctx) {
+        this.pos = object.pos;
+        this.size = object.size;
+        this.color = object.color;
+        this.alive = "yes"
     }
     draw(ctx) {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.pos[0], this.pos[1],this.size[0],this.size[1]);
     }
+    shoot() {
+
+    }
 }
+export default Alien;
